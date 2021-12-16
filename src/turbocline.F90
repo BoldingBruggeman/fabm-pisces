d8d34b8316e77d7d4f618997ea6944f0bcc33b54
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2021-12-16 13:34:09 +0000

    corrected diffusivity units

--- src/turbocline.F90
+++ src/turbocline.F90
@@ -26,14 +26,14 @@ contains
       class (type_pisces_turbocline), intent(inout), target :: self
       integer,                        intent(in)            :: configunit
 
-      call self%get_parameter(self%avt_c, 'avt_c', 'm s-1', 'critical vertical diffusivity', default=5.e-4_rk)
+      call self%get_parameter(self%avt_c, 'avt_c', 'm2 s-1', 'critical vertical diffusivity', default=5.e-4_rk)
       call self%get_parameter(self%minh, 'minh', 'm', 'minimum thickness', default=10._rk)
 
       call self%register_diagnostic_variable(self%id_hmld, 'hmld', 'm', 'turbocline depth', &
          standard_variable=mixed_layer_thickness_defined_by_vertical_tracer_diffusivity, source=source_do_column)
       call self%register_dependency(self%id_gdept_n, standard_variables%depth)
       call self%register_dependency(self%id_bdepth, standard_variables%bottom_depth)
-      call self%register_dependency(self%id_avt, type_interior_standard_variable(name='vertical_tracer_diffusivity', units='m s-1'))
+      call self%register_dependency(self%id_avt, type_interior_standard_variable(name='vertical_tracer_diffusivity', units='m2 s-1'))
    end subroutine initialize
 
    subroutine do_column(self, _ARGUMENTS_DO_COLUMN_)
